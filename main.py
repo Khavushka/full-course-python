479257be46f28a368ba1f34d2bad4d8e85ce4121
Khavushka <khavushka@gmail.com>
2023-03-12 09:39:28 +0100

    Øvelser

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-# Основные типы в Python
+# Несуществующие ключи и метод get
+my_moto = {
+    'brand' : 'Ducati',
+    'price' : 25000,
+    'engine' : 1.2,
+}
+
+print(my_moto["engine"])
+print(my_moto.get('engine', 0))
+
+
+
 

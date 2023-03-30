7777a409c39ae016f4bc430cadbd281041423870
Khavushka <khavushka@gmail.com>
2023-03-30 22:51:05 +0200

    new commit

--- func.py
+++ func.py
@@ -94,6 +94,22 @@ def mult_by_factor(value, multiplier=1):
 
 print(mult_by_factor(10, 2))
 print(mult_by_factor(5))
-# nalichie znacheniya på default for parametres without requiers
+# nalichie znacheniya på default for parametres can using without requiers
 
+from datetime import date
 
+def get_weekday():
+    return date.today().strftime('%A')
+
+def create_new_post(post, weekday=get_weekday()):
+    post_copy = post.copy()
+    post_copy['created_on_weekday'] = weekday
+    return post_copy
+
+initial_post = {
+    'id': 123,
+    'author': 'Fifa'
+}
+
+post_with_weekday = create_new_post(initial_post)
+print(post_with_weekday)

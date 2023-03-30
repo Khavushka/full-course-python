eba1ac4eb167d4789dee2e8332eddc28302d1237
Khavushka <khavushka@gmail.com>
2023-03-30 22:58:49 +0200

    new commit

--- func.py
+++ func.py
@@ -98,13 +98,16 @@ print(mult_by_factor(5))
 
 from datetime import date
 
+
 def get_weekday():
     return date.today().strftime('%A')
+
 
 def create_new_post(post, weekday=get_weekday()):
     post_copy = post.copy()
     post_copy['created_on_weekday'] = weekday
     return post_copy
+
 
 initial_post = {
     'id': 123,
@@ -112,4 +115,5 @@ initial_post = {
 }
 
 post_with_weekday = create_new_post(initial_post)
+print(initial_post)
 print(post_with_weekday)

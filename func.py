2012f26fa4032ef20d93d6511f31e3a5a91e97ee
Khavushka <khavushka@gmail.com>
2023-04-01 00:49:46 +0200

    Callback func

--- func.py
+++ func.py
@@ -122,3 +122,10 @@ print(post_with_weekday)
 
 # ---------------------------------------------------------------------
 # Колбэк функции - Callback function
+def other_fn():
+    pass
+
+def fn_with_callback(callback_fn):
+    callback_fn()
+    
+fn_with_callback(other_fn)
